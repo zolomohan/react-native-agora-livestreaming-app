7bdfe1789e4ac4d98a36f42be815b95c24facf61
zolomohan <mohanzolomon@gmail.com>
2020-11-08 01:38:14 +0530

    Add Agora Instance Create and Destroy

--- screens/Live.js
+++ screens/Live.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RtcEngine from 'react-native-agora';
 
-export default function Live() {
+export default function Live(props) {
+  const AgoraEngine = useRef();
+  const init = async () => {
+    AgoraEngine.current = await RtcEngine.create(
+      'c7e742d5df23478285a9dc4f4ff62407',
+    );
+  };
+
+  useEffect(() => {
+    init();
+    return () => {
+      AgoraEngine.current.destroy();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Live</Text>
