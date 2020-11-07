79c58c1fb543c83de962c57de9cf591f4deb1820
zolomohan <mohanzolomon@gmail.com>
2020-11-08 00:30:34 +0530

    Add Homescreen Buttons

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Livestream App</Text>
+      <View style={styles.createContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Start</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.joinContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Join</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 50,
+    color: '#333',
+  },
+  createContainer: {
+    width: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  joinContainer: {
+    width: '90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    paddingTop: 50,
+    borderTopWidth: 1,
+    borderColor: '#22222255',
+  },
+  button: {
+    width: '100%',
+    marginTop: 15,
+    borderRadius: 8,
+    paddingVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#78b0ff',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
