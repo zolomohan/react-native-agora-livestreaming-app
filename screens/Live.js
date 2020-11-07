29c82b18f2cc2511fe4212241ec83a825196962e
zolomohan <mohanzolomon@gmail.com>
2020-11-08 01:39:09 +0530

    Add Permission Capture for Android

--- screens/Live.js
+++ screens/Live.js
@@ -1,6 +1,27 @@
 import React, { useEffect, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PermissionsAndroid } from 'react-native';
 import RtcEngine from 'react-native-agora';
+
+async function requestCameraAndAudioPermission() {
+  try {
+    const granted = await PermissionsAndroid.requestMultiple([
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+    ]);
+    if (
+      granted['android.permission.RECORD_AUDIO'] ===
+        PermissionsAndroid.RESULTS.GRANTED &&
+      granted['android.permission.CAMERA'] ===
+        PermissionsAndroid.RESULTS.GRANTED
+    ) {
+      console.log('You can use the cameras & mic');
+    } else {
+      console.log('Permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
 
 export default function Live(props) {
   const AgoraEngine = useRef();
@@ -11,6 +32,7 @@ export default function Live(props) {
   };
 
   useEffect(() => {
+    if (Platform.OS === 'android') requestCameraAndAudioPermission();
     init();
     return () => {
       AgoraEngine.current.destroy();
