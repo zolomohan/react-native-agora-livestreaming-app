6e54ded09a4ba9a40003e1ebf921e1ade050090d
zolomohan <mohanzolomon@gmail.com>
2020-11-08 01:12:40 +0530

    Add Navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './screens/Home';
+import Live from './screens/Live';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Text></Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Live" component={Live} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
